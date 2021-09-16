ecd2e80226a3294fdeae3199ab47c6c4c2b0b156
Victor Marri <victor.marri@dtidigital.com.br>
2021-09-15 23:23:47 -0300

    Desafio de metodo concluido

--- Challenges/method.rb
+++ Challenges/method.rb
@@ -4,4 +4,16 @@ Crie um programa que possua um metodo que resolva a potencia dado um numero base
 Esses dois valores tem de ser informados pelo usuario
 
 =end
+resultado = ''
+
+puts 'Digite o numero base: '
+numero_base = gets.chomp.to_i
+puts 'Digite a quantas vezes esse numero vai ser elevado: '
+expoente = gets.chomp.to_i
+
+def potencia(numero_base, expoente)
+  resultado = numero_base ** expoente
+end
+
+puts "O resultado da potencia de #{numero_base} elevado à #{expoente} resulta em: #{potencia(numero_base, expoente)}"
 
